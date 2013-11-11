9dcbc7f1ba93c374157f7029974eeedc55f5a820
Gustavo <gustavo@gustavo-desktop.(none)>
2013-11-11 15:33:13 -0600

    update 11/10/2013 con bug en open

--- consola.asm
+++ consola.asm
@@ -8,6 +8,7 @@ sys_exit        equ     1
 sys_read        equ     3
 sys_write       equ     4
 sys_open		equ		5
+sys_close		equ 	6
 stdin           equ     0
 stdout          equ     1
 
@@ -22,16 +23,19 @@ section .bss
 ;seccion de datos inicializados
 section .data
 
-msj: db "james@aguacate.console>>>  "
+msj: db "james@aguacate.console>>>  " ;mensaje de cabecera en consola
 len: equ $-msj
 
-msjError: db "No se reconoce el comando",10
-errorLen: equ $-msjError
+msjError: db "No se reconoce el comando",10 ;mensaje de error en caso de que no se reconozca
+errorLen: equ $-msjError					;un comando
 
-salir: db "salir"
+msjError2: db "No se pudo abrir el archivo",10 ;mensaje de error en caso de que no abra
+errorLen2: equ $-msjError2					;un archivo
+
+salir: db "salir";texto para usar de comparación para el comando salir
 salirLen: equ $-salir
 
-mostrar: db "mostrar"
+mostrar: db "mostrar";texto para usar de comparación para el comando mostrar
 mostrarLen: equ $-mostrar
 
 archivoPin:	db "a.txt",0
@@ -54,64 +58,78 @@ _start:
 	mov edx, comandoLen;muevo al edx el largo del mensaje
 	mov ecx, comando; muevo al ecx el puntero del mensaje
 	call ReadText; llamo a la subrutina de leer de usuario
-	
-	
-	;mov ecx, 0
-	;dec eax
-	
-;cicloAux:
-
-;	mov	dl, byte [comando + ecx]
-;	inc	ecx		
-;	cmp	ecx, eax		
-;	jne	cicloAux
-;	inc ecx
-;	mov byte [comando + ecx], 0
-
+	dec eax;decremento el eax que trae la cantidad de digitos leidos mas el ENTER
+	;push eax; salvo esa cantidad de dígitos en la pila
 	mov ecx, 0;muevo al ecx un cero que me servira de contador de digitos
-	;dec eax; en el eax queda la cantidad de digitos leidos, incluido el ENTER, 
-		   ; asi que decremento en uno para quitar el ENTER y que me quede solo la cantidad de caracteres leidos
 		   
 ;ciclo principal de la funcion	
 cicloMostrar:
-	mov eax, 7
-	mov	dl, byte [comando + ecx]
-	cmp	dl, byte [mostrar + ecx]
-	jne cicloSalir
-	inc		ecx		
-	cmp		ecx, eax
-	je Show
-	jmp     cicloMostrar
-	
+	mov eax, 7;cantidad de digitos maxima de salir
+	mov	dl, byte [comando + ecx]; si es igual muevo al dl el byte numero ecx(contador) de lo digitado 
+	cmp	dl, byte [mostrar + ecx]; comparo con lo mismo pero en el texto de comparacion
+	jne cicloSalir; si no son iguales pase al otro comando
+	inc		ecx		; si son iguales, incremento el ecx para pasar al otro digito
+	cmp		ecx, eax; comparo el contador con la cantidad de digitos maxima a comparar
+	je Show; si son iguales valla a mostrar el archivo
+	jmp     cicloMostrar; si no siga el ciclo
+;muestra el archivo leido
 Show:
-	mov		ebx, archivoPin
-	mov		ecx, 0		
-	mov		eax,sys_open
-	int		80h		
-	push eax
-	call Muestra
-	pop ebx
-	call Cerrar
-	jmp _start
+	inc ecx ;incremeto el eax para seguir leyendo el comando
+	mov	dl, byte [comando + ecx]; muevo al dl el digito+ecx del archivo
+	cmp dl, 10; lo comparo con enter, para verificar que termino de escribir
+	je .sub2; brinco si es igual
+	mov byte[archivo+ecx], dl; si no lo meto en un buffer
+	jmp Show; y vuelvo al ciclo
 	
-cicloSalir:
-	mov eax, 5
-	mov	dl, byte [comando + ecx]
-	cmp	dl, byte [salir + ecx]
-	jne mensajeError
-	inc		ecx		
-	cmp		ecx, eax
-	je 		Fin
-	jmp     cicloSalir
+.sub2:
+	inc ecx; incremento el ecx
+	mov byte[archivo+ecx], 0; y meto en el buffer un null
 	
-
-mensajeError:
-	mov edx, errorLen    
-	mov ecx, msjError
+	;%%%%%%%%%%%      PRUEBA PARA VER QUE TIENE EL BUFFER %%%%%%%%%%%
+	mov edx, archivoLen
+	mov ecx, archivo
 	call DisplayText
+	;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	
-	jmp _start
+	;mov ebx, archivoPin ; ACA SI LO ABRE
 	
+	mov	ebx, archivo ; muevo al ebx el pintero del archivo (nombre.txt)
+	mov	ecx, 0	; cero para las FLAGS
+	mov	eax,sys_open; llamada al sistema
+	int	80h		
+	push eax;salvo en la pila este FD
+	call Muestra; llamo a la subrutina de mostrar
+	pop ebx	; saco de la pila ese FD
+	call Cerrar ; llamo a cerrar el archivo para que no quede abierto
+	jmp _start; iniacia nuevamente el programa (parecido a un while true)
+	
+;ciclo que revisa lo digitado por el usuario y si este digita salir, se sale del programa
+cicloSalir:
+	mov eax, 5;cantidad de digitos maxima de salir
+	mov	dl, byte [comando + ecx]; si es igual muevo al dl el byte numero ecx(contador) de lo digitado 
+	cmp	dl, byte [salir + ecx]; comparo con lo mismo pero en el texto de comparacion
+	jne mensajeError; si no son iguales salgo al mensaje de error
+	inc		ecx	; si son iguales, incremento el ecx para pasar al otro digito
+	cmp		ecx, eax; comparo el contador con la cantidad de digitos maxima a comparar
+	je 		Fin; si son iguales brinque a salir
+	jmp     cicloSalir; no son iguales siga el ciclo
+	
+; etiqueta de inciao para imprimir el mensaje de error
+mensajeError:
+	mov edx, errorLen; muevo al edx el len del tamaño del mensaje de error
+	mov ecx, msjError; muevo al ecx el puntero del mensaje
+	call DisplayText; llamo a la subrutina de mostrar en pantalla
+	
+	jmp _start ; si se imprime mensaje de error, inicie nuevamente el programa (parecido a un while true)
+	
+mensajeError2:
+	mov edx, errorLen2; muevo al edx el len del tamaño del mensaje de error
+	mov ecx, msjError2; muevo al ecx el puntero del mensaje
+	call DisplayText; llamo a la subrutina de mostrar en pantalla
+	
+	jmp _start ; si se imprime mensaje de error, inicie nuevamente el programa (parecido a un while true)
+;fin del programa
+; paso al eax 1 y ebx 0 y me salgo con la llamada al sistema
 Fin:
 	mov eax, sys_exit
 	mov ebx, 0
@@ -138,30 +156,25 @@ ReadText:
     mov     eax, sys_read
     int     80H
     ret
-    
+ 
+;subrutina que muestra el contenido interno de un archivo 
 Muestra:
-	;mov		ebx, archivoPin
-	;mov		ecx, 0		
-	;mov		eax,sys_open
-	;int		80h		
-	
-
-	test	eax, eax
-	js	mensajeError
-
-; Leo el pin del archivo
-	mov		ebx, eax
-	mov		ecx, archivo
-	mov		edx, archivoLen
-	mov		eax, sys_read				
+	test	eax, eax ; primero nos aseguramos que abrio bien
+	js	mensajeError2; no es asi? imprime mensaje de errorLen
+	; si se abre bien
+	mov		ebx, eax; paso al ebx el FD
+	mov		ecx, archivo; paso el puntero del buffer con el archivo
+	mov		edx, archivoLen; y su len correspondiente
+	mov		eax, sys_read; y llamo a read de dicho archivo
 	int 	80h		
-	
+	;Una vez leido lo imprimo en consola por medio del DisplayText
 	mov edx, archivoLen
 	mov ecx, archivo 
 	call DisplayText	
 	ret
-	
+
+;Subrutina que cierrra un archivo abierto
 Cerrar:
-	mov eax, 6
+	mov eax, sys_close; solamente se llama a la llamada del sistema sys_close
 	int 80h
 	ret
